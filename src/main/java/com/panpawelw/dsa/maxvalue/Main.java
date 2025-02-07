278f60b49ce19b5c7cfe1ca30c94e7b82707dc88
Paweł Wojtuń <panpawelw@protonmail.com>
2025-02-07 23:49:05 +0100

    Finish maxvalue

--- src/main/java/com/panpawelw/dsa/maxvalue/Main.java
+++ src/main/java/com/panpawelw/dsa/maxvalue/Main.java
@@ -2,6 +2,13 @@ package com.panpawelw.dsa.maxvalue;
 
 public class Main {
     public static void main(String[] args) {
-
+        int maximum = Integer.MIN_VALUE;
+        int[] numbers = new int[]{1,2,3,4,5};
+        for (int number : numbers) {
+            if (number > maximum) {
+                maximum = number;
+            }
+        }
+        System.out.println("Maximum value is " + maximum);
     }
 }
