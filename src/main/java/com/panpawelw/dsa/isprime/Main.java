9aa4af8b9dcc9f53ee960d636073be72cf6c80a3
Paweł Wojtuń <panpawelw@protonmail.com>
2025-02-17 23:55:50 +0100

    Add isprime()

--- src/main/java/com/panpawelw/dsa/isprime/Main.java
+++ src/main/java/com/panpawelw/dsa/isprime/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
