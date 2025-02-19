83439bd484ccc752e07da801e0a205cff6e1f627
Paweł Wojtuń <panpawelw@protonmail.com>
2025-02-19 23:55:53 +0100

    Finish isprime

--- src/main/java/com/panpawelw/dsa/isprime/Main.java
+++ src/main/java/com/panpawelw/dsa/isprime/Main.java
@@ -1,8 +1,17 @@
 package com.panpawelw.dsa.isprime;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        while(!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        int number = scanner.nextInt();
+        System.out.printf("%d is %sa prime number\n", number, isPrime(number) ?"":"not ");
+        scanner.close();
     }
 
     public static boolean isPrime(int number) {
