dc68065cc4f9fa51d7c47767d30630e1ceac0bbe
Paweł Wojtuń <panpawelw@protonmail.com>
2025-02-23 14:09:52 +0100

    Refactor maxvalue

--- src/main/java/com/panpawelw/dsa/maxvalue/Main.java
+++ src/main/java/com/panpawelw/dsa/maxvalue/Main.java
@@ -1,9 +1,11 @@
 package com.panpawelw.dsa.maxvalue;
 
 public class Main {
+
+    public static final int[] numbers = new int[]{1,2,7,4,5};
+
     public static void main(String[] args) {
         int maximum = Integer.MIN_VALUE;
-        int[] numbers = new int[]{1,2,3,4,5};
         for (int number : numbers) {
             if (number > maximum) {
                 maximum = number;
