d8fd07bca17eff50f172b070f96c6a9257a2cfc1
Paweł Wojtuń <panpawelw@protonmail.com>
2025-02-24 23:58:07 +0100

    Finish sumrecursive

--- src/main/java/com/panpawelw/dsa/sumrecursive/Main.java
+++ src/main/java/com/panpawelw/dsa/sumrecursive/Main.java
@@ -1,7 +1,19 @@
 package com.panpawelw.dsa.sumrecursive;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+
+    public static final int[] INT_ARRAY = {1,2,3,4,5};
+
+    public static void main(String[] args) {
+        System.out.println(sumArray(INT_ARRAY));
+    }
+
+    static int sumArray(int[] array) {
+        if (array.length == 1) {
+            return array[0];
+        }
+        return array[0] + sumArray(Arrays.copyOfRange(array, 1, array.length));
     }
 }
