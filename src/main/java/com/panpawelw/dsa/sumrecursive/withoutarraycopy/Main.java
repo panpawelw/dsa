f99aab5aca9675899d91c0400275459dcb51ba52
Paweł Wojtuń <panpawelw@protonmail.com>
2025-03-04 00:56:25 +0100

    Add recursive sum without copying the array

---
+++ src/main/java/com/panpawelw/dsa/sumrecursive/withoutarraycopy/Main.java
@@ -0,0 +1,17 @@
+package com.panpawelw.dsa.sumrecursive.withoutarraycopy;
+
+public class Main {
+
+    public static final int[] INT_ARRAY = {1,2,3,4,5};
+
+    public static void main(String[] args) {
+        System.out.println(sumArray(INT_ARRAY, 0));
+    }
+
+    static int sumArray(int[] array, int currentIndex) {
+        if (currentIndex == array.length - 1) {
+            return array[currentIndex];
+        }
+        return array[currentIndex] + sumArray(array, currentIndex + 1);
+    }
+}
