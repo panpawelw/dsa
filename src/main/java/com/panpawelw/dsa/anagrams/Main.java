b9ed50301d828988f8596879a4fc0997fcdf9def
Paweł Wojtuń <panpawelw@protonmail.com>
2025-03-08 23:50:00 +0100

    Finish anagrams

--- src/main/java/com/panpawelw/dsa/anagrams/Main.java
+++ src/main/java/com/panpawelw/dsa/anagrams/Main.java
@@ -1,7 +1,22 @@
 package com.panpawelw.dsa.anagrams;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Main {
+
+    public static final String STRING_ONE = "restful";
+    public static final String STRING_TWO = "fluster";
+
+    public static void main(String[] args) {
+        System.out.println(charCount(STRING_ONE).equals(charCount(STRING_TWO))?
+                "These strings are anagrams.":"These strings are not anagrams.");
+    }
+
+    public static HashMap<Character, Integer> charCount(String input) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            charCount.merge(input.charAt(i), 1, Integer::sum);
+        }
+        return charCount;
     }
 }
