9a97d75b256799c5582fdb0ff1d88e44f4a392e2
Paweł Wojtuń <panpawelw@protonmail.com>
2025-03-09 23:59:13 +0100

    Add mostfrequentchar stub

---
+++ src/main/java/com/panpawelw/dsa/mostfrequentchar/Main.java
@@ -0,0 +1,10 @@
+package com.panpawelw.dsa.mostfrequentchar;
+
+public class Main {
+
+    public static final String TEST_STRING = "Test string";
+
+    public static void main(String[] args) {
+
+    }
+}
