545d993742db204e1d17d0f01f4d5cb45f1b6759
Paweł Wojtuń <panpawelw@protonmail.com>
2025-03-15 21:06:41 +0100

    Add character count to MostFrequentChar

--- src/main/java/com/panpawelw/dsa/mostfrequentchar/MostFrequentChar.java
+++ src/main/java/com/panpawelw/dsa/mostfrequentchar/MostFrequentChar.java
@@ -1,10 +1,15 @@
 package com.panpawelw.dsa.mostfrequentchar;
 
+import java.util.HashMap;
+
 public class MostFrequentChar {
 
     public static final String TEST_STRING = "Test string";
 
     public static void main(String[] args) {
-
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < TEST_STRING.length(); i++) {
+            charCount.merge(TEST_STRING.charAt(i), 1, Integer::sum);
+        }
     }
 }
