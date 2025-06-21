2bfdd6c73346f80497decd24bcfe8de917e1bee1
Paweł Wojtuń <panpawelw@protonmail.com>
2025-06-21 21:04:07 +0200

    chore: add Javadoc

--- src/main/java/com/panpawelw/dsa/sumrecursive/withoutarraycopy/SumRecursive.java
+++ src/main/java/com/panpawelw/dsa/sumrecursive/withoutarraycopy/SumRecursive.java
@@ -1,5 +1,10 @@
 package com.panpawelw.dsa.sumrecursive.withoutarraycopy;
 
+/**
+ * Adds up all the integers in an array using recursion.
+ *
+ * @author panpawelw
+ */
 public class SumRecursive {
 
   public static final int[] INT_ARRAY = {1,2,3,4,5};
@@ -8,6 +13,13 @@ public class SumRecursive {
     System.out.println(sumArray(INT_ARRAY, 0));
   }
 
+  /**
+   * Adds up all the integers in an array using recursion.
+   * Array is not being copied in the process, array index is used instead.
+   *
+   * @param array array of integers to be added up.
+   * @return      sum.
+   */
   static int sumArray(int[] array, int currentIndex) {
     if (currentIndex == array.length - 1) {
       return array[currentIndex];
