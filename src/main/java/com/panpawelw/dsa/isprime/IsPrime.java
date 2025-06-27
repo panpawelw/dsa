577b44ea7ec5af7a8916d961b8fd38a63f72143b
Paweł Wojtuń <panpawelw@protonmail.com>
2025-06-28 00:34:24 +0200

    refactor: improve performance

--- src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
+++ src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
@@ -26,7 +26,7 @@ public class IsPrime {
    */
   public static boolean isPrime(int number) {
     if (number < 2) return false;
-    for (int i = 2; i < number; i++) {
+    for (int i = 2; i < Math.sqrt(number); i++) {
       if (number % i == 0) return false;
     }
     return true;
