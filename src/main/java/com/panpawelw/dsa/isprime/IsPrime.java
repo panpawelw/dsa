05c893ffb85edcaadf9c208cfefc58cdeff63b5c
Paweł Wojtuń <panpawelw@protonmail.com>
2025-06-27 23:58:53 +0200

    refactor: improve edge case handling

--- src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
+++ src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
@@ -25,6 +25,7 @@ public class IsPrime {
    * @return        boolean response.
    */
   public static boolean isPrime(int number) {
+    if (number < 2) return false;
     for (int i = 2; i < number; i++) {
       if (number % i == 0) return false;
     }
