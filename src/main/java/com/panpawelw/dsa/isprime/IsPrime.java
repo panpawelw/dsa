da305ffbf3ab7203a4a39dc188a91c7a8c67f7ec
Paweł Wojtuń <panpawelw@protonmail.com>
2025-07-19 23:51:49 +0200

    fix: remove a bug causing some false positives

--- src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
+++ src/main/java/com/panpawelw/dsa/isprime/IsPrime.java
@@ -33,7 +33,7 @@ public class IsPrime {
    */
   public static boolean isPrime(int number) {
     if (number < 2) return false;
-    for (int i = 2; i < Math.sqrt(number); i++) {
+    for (int i = 2; i <= Math.sqrt(number); i++) {
       if (number % i == 0) return false;
     }
     return true;
